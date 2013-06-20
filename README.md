ca57acdb7f5abb55f5a430222c91897c39bd8df3
Jack Sullivan <john.t.sullivan@gmail.com>
2013-06-20 16:33:46 -0400

    Updated to 0.3 - changed to read out lists of vertices and edges updated readme accordingly.

--- README.md
+++ README.md
@@ -4,5 +4,33 @@ Concrete-Kelvin
 Tools to map between Kelvin output and Concrete
 
 To convert a file from Kelvin to Concrete run the following command (replacing the directory file with your directory file):
-mvn scala:run -Dlauncher=kelvin2concrete -DkelvinFile=/postsmall/postsmall.tac11
 
+`mvn scala:run -Dlauncher=kelvin2concrete -DKelvinFile=<my file name>`
+
+The system will write a file out to `<my file name>.pb` that will contain:
+
+1. An int representing the number of vertices in the protobuf
+2. The appropriate number of vertices
+3. An int representing the number of edges in the protobuf
+4. The appropriate number of edges
+
+It probably makes the most sense to read in this data like so:
+
+```scala
+import java.io.{DataInputStream, FileInputStream}
+import edu.jhu.hlt.concrete.{Vertex, Edge}
+
+object App {
+  val stream = new DataInputStream(new FileInputStream("kelvinfile.pb"))
+
+  val vertices:mutable.ArrayBuffer[Vertex] = new mutable.ArrayBuffer[Vertex]()
+  val edges:mutable:ArrayBuffer[Edge] = new mutable.ArrayBuffer[Edge]()
+
+  for(idex <- 0 until stream.readInt) {
+    vertices += Vertex.parseFrom(stream)
+  }
+
+  for(idex <- 0 until stream.readInt) {
+    edges += Edge.parseFrom(stream)
+  }
+}
--- src/main/scala/edu/jhu/hlt/concrete/converters/kelvin/Reader.scala
+++ src/main/scala/edu/jhu/hlt/concrete/converters/kelvin/Reader.scala
@@ -1,9 +1,9 @@
 package edu.jhu.hlt.concrete.converters.kelvin
 
 import java.util.logging.Logger
-import java.io.FileOutputStream
+import java.io.{DataOutputStream, FileOutputStream}
 import scala.io.Source
-import edu.jhu.hlt.concrete.Concrete.KnowledgeGraph
+import edu.jhu.hlt.concrete.Concrete.{Edge, Vertex}
 import edu.jhu.hlt.concrete.converters.kelvin.CommunicationTraits.{Vertexable, Edgeable}
 import edu.jhu.hlt.concrete.converters.kelvin.AttributeConversions._
 import scala.collection.JavaConverters._
@@ -12,25 +12,24 @@ import scala.collection.JavaConverters._
  * @author John Sullivan
  */
 object Reader {
-  def apply(filename:String):KnowledgeGraph = {
+  def apply(filename:String):(Iterable[Vertex], Iterable[Edge]) = {
     val lines:Seq[KelvinLine] = (Source.fromFile(filename).getLines map {KelvinLine}).flatten.toSeq
     val edgeLines:Iterable[EdgeMention] = EdgeMention(lines.collect{case l:Edgeable => l})
     val vertexLines:Iterable[VertexMention] = VertexMention(lines.collect{case l:Vertexable => l})
-    KnowledgeGraph.newBuilder
-      .setUuid(genUUID)
-      .addAllVertex((vertexLines map {_.toMessage}).asJava)
-      .addAllEdge((edgeLines map {_.toMessage}).asJava)
-      .build
+    (vertexLines.map{_.toMessage}, edgeLines.map{_.toMessage})
   }
 
   def main(args:Array[String]) {
     val log = Logger.getLogger(Reader.getClass.getName)
     val filename:String = System.getProperty("KelvinFile")
     log.info("Parsing " + filename)
-    val kg:KnowledgeGraph = Reader(filename)
+    val (vertices, edges) = Reader(filename)
     log.info(filename + " parsed to knowledge graph")
-    val wrt = new FileOutputStream(filename + ".pb")
-    kg.writeTo(wrt)
+    val wrt = new DataOutputStream(new FileOutputStream(filename + ".pb"))
+    wrt.writeInt(vertices.size)
+    vertices.foreach(_.writeTo(wrt))
+    wrt.writeInt(edges.size)
+    edges.foreach(_.writeTo(wrt))
     wrt.flush
     wrt.close
     log.info("Wrote to " + filename + ".pb")
